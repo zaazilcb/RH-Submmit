7b74cdd26201fab699733f1e7fa9ebf0e1b5c9c9
Zaazil Cabreja <114429996+zaazilcb@users.noreply.github.com>
2025-03-24 17:48:35 -0600

    Add files via upload

---
+++ prueba.js
@@ -0,0 +1,34 @@
+/*new Swiper('.card-wrapper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  }); */
+  document.addEventListener("DOMContentLoaded", function () {
+    const titulos = document.querySelectorAll(".titulo");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+        });
+    }, { threshold: 0.3 });
+
+    titulos.forEach(titulo => observer.observe(titulo));
+});
